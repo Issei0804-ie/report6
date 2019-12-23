d96bc411c4b22d4a832b2d84f4270cf8076c027d
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2019-12-23 19:17:07 +0900

    add BindFunc

---
+++ src/main/java/jp/ac/uryukyu/ie/e195755/Server.java
@@ -0,0 +1,22 @@
+package jp.ac.uryukyu.ie.e195755;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class Server {
+    Socket socket = new Socket();
+    public Server(int port){
+        // portの設定
+        InetSocketAddress address = new InetSocketAddress(port);
+
+        //指定されたポートにbindを行う。
+        try{
+            socket.bind(address);
+        }catch(java.io.IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+
